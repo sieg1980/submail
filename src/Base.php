14c739c48c73ed09696f67fe8baca23c129e7907
sieg <sieg@zimutech.com>
2019-09-25 23:56:34 +0800

    修正无法在邮件中添加附件的问题

--- src/Base.php
+++ src/Base.php
@@ -4,79 +4,74 @@ namespace Zimutech;
 
 class Base
 {
-	const BASE_URL = 'https://api.mysubmail.com/';
+    const BASE_URL = 'https://api.mysubmail.com/';
 
-	protected $signType = 'sha1';
-	protected $appId;
-	protected $appKey;
-	protected $smsSubhookKey;
-	protected $emailSubhookKey;
-	
-	function __construct(string $appId, string $appKey, string $smsSubhookKey, string $emailSubhookKey)
-	{
-		$this->appId = $appId;
-		$this->appKey = $appKey;
-		$this->smsSubhookKey = $smsSubhookKey;
-		$this->emailSubhookKey = $emailSubhookKey;
-	}
-	
-	protected function buildSignature(array $request) : string
-	{
-		ksort($request);
-		
-		$tmp = [];
+    protected $signType = 'sha1';
+    protected $appId;
+    protected $appKey;
+    protected $smsSubhookKey;
+    protected $emailSubhookKey;
 
-		foreach($request as $k => $v)
-		{
-			if($k !== 'attachments') {
-				$tmp[] = $k . '=' . $v;
-			}
-		}
-		
-		$arg = implode('&', $tmp);
+    function __construct(string $appId, string $appKey, string $smsSubhookKey, string $emailSubhookKey)
+    {
+        $this->appId = $appId;
+        $this->appKey = $appKey;
+        $this->smsSubhookKey = $smsSubhookKey;
+        $this->emailSubhookKey = $emailSubhookKey;
+    }
 
-		if(get_magic_quotes_gpc()) {
-			$arg = stripslashes($arg);
-		}
+    protected function buildSignature(array $request) : string
+    {
+        ksort($request);
 
-		$result = sha1($this->appId . $this->appKey . $arg . $this->appId . $this->appKey);
+        $tmp = [];
 
-		return $result;
-	}
+        foreach($request as $k => $v)
+        {
+            if(strpos($k, 'attachments') === false) {
+                $tmp[] = $k . '=' . $v;
+            }
+        }
 
-	protected function httpRequest(string $api, array $data, string $method = 'post') : array
-	{
-		if($method === 'post') {
-			$ch = curl_init($api);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/x-www-form-urlencoded"]);
-		} else {
-			$url = $api . '?' . http_build_query($data);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-		}
+        $arg = implode('&', $tmp);
 
-		$output = curl_exec($ch);
-		curl_close($ch);
-		$output = trim($output, "\xEF\xBB\xBF");
-		return json_decode($output, true);
-	}
+        $result = sha1($this->appId . $this->appKey . $arg . $this->appId . $this->appKey);
 
-	protected function getTimestamp() : string
-	{
+        return $result;
+    }
+
+    protected function httpRequest(string $api, array $data, string $method = 'post') : array
+    {
+        if($method === 'post') {
+            $ch = curl_init($api);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        } else {
+            $url = $api . '?' . http_build_query($data);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        }
+
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $output = trim($output, "\xEF\xBB\xBF");
+        return json_decode($output, true);
+    }
+
+    protected function getTimestamp() : string
+    {
         $api = self::BASE_URL . 'service/timestamp.json';
-        
+
         $ch = curl_init($api) ;
-        
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ;
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ;
-        
+
         $output = curl_exec($ch) ;
         $timestamp = json_decode($output, true);
-        
+
         return $timestamp['timestamp'];
     }
 }
--- src/Mail.php
+++ src/Mail.php
@@ -6,18 +6,29 @@ require_once('Base.php');
 
 class Mail extends Base
 {
-	public function send(array $request) : array
-	{
+    public function send(array $request) : array
+    {
         $api = self::BASE_URL . 'mail/send.json';
+
+        if(array_key_exists('attachments', $request)) {
+            $attachments = $request['attachments'];
+
+            for($i = 0; $i < count($attachments); $i++)
+            {
+                $request['attachments['. $i . ']'] = curl_file_create($attachments[$i], mime_content_type($attachments[$i]), $attachments[$i]);
+            }
+
+            unset($request['attachments']);
+        }
 
         $request['appid'] = $this->appId;
         $request['timestamp'] = $this->getTimestamp();
         $request['sign_type'] = 'sha1';
         $request['signature'] = $this->buildSignature($request);
-        
+
         $result = $this->httpRequest($api, $request);
         return $result;
-	}
+    }
 
     public function subhookValidate() : bool
     {
