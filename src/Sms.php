25622bdb4e6db85b70cdabaf89090553fb7725ff
sieg1980 <sieg@zimutech.com>
2022-05-27 22:35:23 +0800

    增加xsend和multiXSend方法，更新短信发送相关API的地址。

--- src/Sms.php
+++ src/Sms.php
@@ -8,7 +8,7 @@ class Sms extends Base
 {
     public function send(string $to, string $content, string $tag = '') : array
     {
-        $api = 'message/send.json';
+        $api = 'sms/send.json';
 
         $request['to'] = $to;
         $request['content'] = $content;
@@ -21,12 +21,43 @@ class Sms extends Base
         return $this->httpRequest($api, $request);
     }
 
+    public function xsend(string $to, string $projectId, array $vars, string $tag = '')
+    {
+        $api = 'sms/xsend.json';
+
+        $request['to'] = $to;
+        $request['project'] = $projectId;
+        $request['vars'] = json_encode($vars);
+        $request['signature'] = $this->buildSignature($request);
+
+        if(!empty($tag)) {
+            $request['tag'] = $tag;
+        }
+
+        return $this->httpRequest($api, $request);
+    }
+
     public function multiSend(array $to, string $content, string $tag = '') : array
     {
-        $api = 'message/multisend.json';
+        $api = 'sms/multisend.json';
 
         $request['multi'] = json_encode($to);
         $request['content'] = $content;
+        $request['signature'] = $this->buildSignature($request);
+
+        if(!empty($tag)) {
+            $request['tag'] = $tag;
+        }
+
+        return $this->httpRequest($api, $request);
+    }
+
+    public function multiXSend(array $to, string $projectId, array $multi, $tag = '')
+    {
+        $api = 'sms/multixsend.json';
+
+        $request['project'] = $projectId;
+        $request['multi'] = json_encode($multi);
         $request['signature'] = $this->buildSignature($request);
 
         if(!empty($tag)) {
